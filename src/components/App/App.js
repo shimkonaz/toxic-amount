df91ec9c218c0b2aad4778bf88f369eeec92f0e0
shimkonaz <shimkonaz@gmail.com>
2018-11-29 02:29:18 +0200

    implemented list item structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
 import UploadArea from '../UploadArea/UploadArea';
+import ItemsList from '../ItemsList';
 
 import './App.scss';
 
 class App extends Component {
+  state = {
+    mockItems: [
+      {
+        id: 1,
+        itemName: 'one',
+        itemDescription: 'one_descr'
+      },
+      {
+        id: 2,
+        itemName: 'two',
+        itemDescription: 'two_descr'
+      },
+      {
+        id: 3,
+        itemName: 'three',
+        itemDescription: 'three_descr'
+      }
+    ]
+  }
 
   render() {
+
     return (
       <div className="App">
         <UploadArea>
         </UploadArea>
-        <ItemsList>
-        </ItemsList>
+        <ItemsList items={this.state.mockItems}/>
       </div>
     );
   }
--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -5,8 +5,25 @@ import './ItemsList.scss';
 class ItemsList extends Component {
 
   render() {
+    const { items } = this.props;
+    const itemsList = items.map(item => {
+      return (
+        <div 
+          className="itemInfo" 
+          key={item.id}>
+          <div className="item-name">
+            Name: {item.itemName}
+          </div>
+          <div className="item-descr">
+            Description: {item.itemDescription}
+          </div>
+        </div>
+      )
+    });
+
     return (
       <div className="itemsList">
+        { itemsList }
       </div>
     );
   }
