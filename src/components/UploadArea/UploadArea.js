c02b8cf435862d26730601090703f1aa9fe6cfcb
shimkonaz <shimkonaz@gmail.com>
2018-12-02 00:38:22 +0200

    implemented image loading

--- src/components/UploadArea/UploadArea.js
+++ src/components/UploadArea/UploadArea.js
@@ -4,15 +4,56 @@ import { UploadArrow } from '../Icons';
 import './UploadArea.scss';
 
 class UploadArea extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: null
+    }
+  }
+
+  // tessaractRes.filter(item => item)
+
+  // componentWillUnmount() {
+
+  // url.revokeObjectUrl
+  // }
   
+  handleAreaClick = () => {
+    this.inputElement.click();
+  }
+  
+  handleFileSelection = (e) => {
+    this.setState({
+      file: URL.createObjectURL(e.target.files[0])
+    })
+  }
+
   render() {
     return (
-      <div className="uploadArea">
+      <div 
+        className="uploadArea"
+        onClick={this.handleAreaClick}
+        style={{
+          backgroundImage: 'url(' + this.state.file + ')', 
+          backgroundSize: 'cover', 
+          backgroundPosition: 'center center',
+          backgroundRepeat: 'no-repeat',
+        }}>
+
+        <input
+          onChange={this.handleFileSelection}
+          type="file"
+          ref={input => this.inputElement = input}
+          style={{display: 'none'}} />
+        
         <div className="uploadArea__icon">
           <UploadArrow />
         </div>
+
         <div className="uploadArea__title">
-          To upload, click here <br/> or <br/> drop files in this area
+          To upload, click here <br/> or <br/>
+           drop files in this area
         </div>
       </div>
     );
