1cd93fa574eb08409252068f7badf9c0a0028aa1
shimkonaz <shimkonaz@gmail.com>
2018-12-13 11:18:39 +0200

    updated harms id's

--- src/consts.js
+++ src/consts.js
@@ -18,73 +18,73 @@ export const HARMS = [
     In laboratory experiments, exposure to high doses of 2-BE has been shown 
     to cause reproductive problems.`
   },
-  // {
-  //   id: 3,
-  //   title: 'monoethanalomine',
-  //   description: `Hormone-disrupting chemicals that can form cancer-causing nitrates. 
-  //   A skin, eye and respiratory irritant.`
-  // },
-  // {
-  //   id: 4,
-  //   title: 'Nonylphenol ethoxylate',
-  //   description: `Degrade into nonylphenols (NPs), which can mimic the hormone estrogen. 
-  //   In laboratory experiments, NP has been shown to stimulate the growth of human breast 
-  //   cancer cells and cause adverse reproductive effects in fish and other aquatic organisms.`
-  // },
-  // {
-  //   id: 5,
-  //   title: 'Phosphate',
-  //   description: `Environmental hazard causing excessive growth in aquatic plants which 
-  //   leads to the suffocation of fish & underwater life.`
-  // },
-  // {
-  //   id: 6,
-  //   title: 'Quaternary Ammonium Compound',
-  //   description: `Irritant and sensitizer that can induce an allergic response following 
-  //   contact with the skin. Quats are also known to cause occupational asthma in cleaning 
-  //   workers and preliminary evidence indicates they may cause adverse genetic and 
-  //   reproductive effects.`
-  // },
-  // {
-  //   id: 7,
-  //   title: 'Sodium dichloroisocyanurate dehydrate',
-  //   description: `Corrosive, severe eye, skin and respiratory irritant. It can also form 
-  //   chlorine gas, which will burn the eyes, nose and mouth. Studies have found that high 
-  //   doses of this chemical cause kidney damage.`
-  // },
-  // {
-  //   id: 8,
-  //   title: 'Sodium hydroxide',
-  //   description: `Highly corrosive; can burn the eyes, skin and lungs and is a respiratory
-  //    irritant. Long-term exposure in the air may lead to ulceration of the nasal passages 
-  //    and chronic skin irritation.`
-  // },
-  // {
-  //   id: 9,
-  //   title: 'Sodium lauryl sulfate',
-  //   description: ` are detergents that pose serious health threat. Young eyes may 
-  //   not develop properly if expose to SLS or SLES and may damage the skin’s immune system.`
-  // },
-  // {
-  //   id: 10,
-  //   title: 'sodium laureth sulfate',
-  //   description: ` are detergents that pose serious health threat. Young eyes may 
-  //   not develop properly if expose to SLS or SLES and may damage the skin’s immune system.`
-  // },
-  // {
-  //   id: 11,
-  //   title: 'Triclosan',
-  //   description: `Synthetic antibacterial that is toxic and a suspected endocrine 
-  //   disrupter that can mimic or interfere with the function of hormones. There is 
-  //   concern that its extensive use in consumer products is contributing to 
-  //   antibiotic-resistant bacteria.`
-  // },
-  // {
-  //   id: 12,
-  //   title: 'Phenol',
-  //   description: `Extremely caustic chemical that burns the skin. 
-  //   Absorption through the lungs or skin can cause central nervous system damage, 
-  //   pneumonia, respiratory problems, kidney and liver damage, numbness, vomiting 
-  //   and a be fatal.`
-  // }
+  {
+    id: '3',
+    title: 'monoethanalomine',
+    description: `Hormone-disrupting chemicals that can form cancer-causing nitrates. 
+    A skin, eye and respiratory irritant.`
+  },
+  {
+    id: '4',
+    title: 'Nonylphenol ethoxylate',
+    description: `Degrade into nonylphenols (NPs), which can mimic the hormone estrogen. 
+    In laboratory experiments, NP has been shown to stimulate the growth of human breast 
+    cancer cells and cause adverse reproductive effects in fish and other aquatic organisms.`
+  },
+  {
+    id: '5',
+    title: 'Phosphate',
+    description: `Environmental hazard causing excessive growth in aquatic plants which 
+    leads to the suffocation of fish & underwater life.`
+  },
+  {
+    id: '6',
+    title: 'Quaternary Ammonium Compound',
+    description: `Irritant and sensitizer that can induce an allergic response following 
+    contact with the skin. Quats are also known to cause occupational asthma in cleaning 
+    workers and preliminary evidence indicates they may cause adverse genetic and 
+    reproductive effects.`
+  },
+  {
+    id: '7',
+    title: 'Sodium dichloroisocyanurate dehydrate',
+    description: `Corrosive, severe eye, skin and respiratory irritant. It can also form 
+    chlorine gas, which will burn the eyes, nose and mouth. Studies have found that high 
+    doses of this chemical cause kidney damage.`
+  },
+  {
+    id: '8',
+    title: 'Sodium hydroxide',
+    description: `Highly corrosive; can burn the eyes, skin and lungs and is a respiratory
+     irritant. Long-term exposure in the air may lead to ulceration of the nasal passages 
+     and chronic skin irritation.`
+  },
+  {
+    id: '9',
+    title: 'Sodium lauryl sulfate',
+    description: ` are detergents that pose serious health threat. Young eyes may 
+    not develop properly if expose to SLS or SLES and may damage the skin’s immune system.`
+  },
+  {
+    id: '10',
+    title: 'sodium laureth sulfate',
+    description: ` are detergents that pose serious health threat. Young eyes may 
+    not develop properly if expose to SLS or SLES and may damage the skin’s immune system.`
+  },
+  {
+    id: '11',
+    title: 'Triclosan',
+    description: `Synthetic antibacterial that is toxic and a suspected endocrine 
+    disrupter that can mimic or interfere with the function of hormones. There is 
+    concern that its extensive use in consumer products is contributing to 
+    antibiotic-resistant bacteria.`
+  },
+  {
+    id: '12',
+    title: 'Phenol',
+    description: `Extremely caustic chemical that burns the skin. 
+    Absorption through the lungs or skin can cause central nervous system damage, 
+    pneumonia, respiratory problems, kidney and liver damage, numbness, vomiting 
+    and a be fatal.`
+  }
 ]
