012badb98b2219127e3a2ff9df36ac3589cbb346
shimkonaz <shimkonaz@gmail.com>
2018-12-13 19:57:36 +0200

    refined item component view

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import './Item.scss';
+
 const Item = ({match, harms} = this.props) => {
   
   const exactItem = harms.find(
@@ -9,10 +11,14 @@ const Item = ({match, harms} = this.props) => {
     );
 
   return (
-    <div>
-      <Link to="/items">
-        Back to list
-      </Link>
+    <div className="item">
+      <div className="back-nav">
+        <Link to="/items">
+          <i className="material-icons">
+            keyboard_arrow_left
+          </i>
+        </Link>
+      </div>
       <div className="title">
         {exactItem.title}
       </div>
