94246ceb3024d6772ec83a4a00b71427dab66435
shimkonaz <shimkonaz@gmail.com>
2018-12-14 23:37:02 +0200

    removed styles from root index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'styles/base.scss';
-
 import App from 'components/App';
 // import * as serviceWorker from 'serviceWorker';
 import { Provider } from 'react-redux';
