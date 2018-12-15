25724aea59b77d9e286c1b766ad7ef55350e3105
shimkonaz <shimkonaz@gmail.com>
2018-12-15 14:57:34 +0200

    Updates

---
+++ precache-manifest.f03bd6c8803f8c41bb28478931fe2cc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "109a8463eea94cece43d",
+    "url": "/toxic-amount/static/js/runtime~main.109a8463.js"
+  },
+  {
+    "revision": "75f7cee517185818e8ba",
+    "url": "/toxic-amount/static/js/main.75f7cee5.chunk.js"
+  },
+  {
+    "revision": "dbd02ef062b3d947871e",
+    "url": "/toxic-amount/static/js/1.dbd02ef0.chunk.js"
+  },
+  {
+    "revision": "75f7cee517185818e8ba",
+    "url": "/toxic-amount/static/css/main.2ceedfc6.chunk.css"
+  },
+  {
+    "revision": "0894901018cf5d6eb1428c74a4838865",
+    "url": "/toxic-amount/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/toxic-amount/precache-manifest.f03bd6c8803f8c41bb28478931fe2cc7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/toxic-amount/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
