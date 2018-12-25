76d8e827216ae4aee78dded8ceda7eef17d3231d
shimkonaz <shimkonaz@gmail.com>
2018-12-25 15:17:21 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 
-import App from 'components/App';
+import App from './components/App';
 // import * as serviceWorker from 'serviceWorker';
 import { Provider } from 'react-redux';
 import store from './store/createStore';
